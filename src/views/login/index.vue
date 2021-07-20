11abb07c3a7f18298dc4e74d85a19cfc6b5f9854
xiongleixin <leilei@yscredit.com>
2021-07-20 16:06:42 +0800

    adjust: adjsut file format

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="container">
     <el-card id="login">
-      <el-form :model="ruleForm"
-                :rules="rules"
-                ref="ruleForm"
-                label-width="80px"
-                class="demo-ruleForm"
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="80px"
+        class="demo-ruleForm"
       >
-        <el-form-item size="mini">
+        <el-form-item>
           <h3>测试管理台</h3>
         </el-form-item>
-        <el-form-item prop="username" size="mini">
-          <el-input v-model="ruleForm.username" prefix-icon="el-icon-user" size="mini" placeholder="请输入帐号">
+        <el-form-item prop="username">
+          <el-input
+            v-model="ruleForm.username"
+            prefix-icon="el-icon-user"
+            placeholder="请输入帐号"
+          >
           </el-input>
         </el-form-item>
-        <el-form-item prop="password" size="mini">
-          <el-input type="password" v-model="ruleForm.password" prefix-icon="el-icon-lock" size="mini" placeholder="请输入密码">
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            prefix-icon="el-icon-lock"
+            placeholder="请输入密码"
+          >
           </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%" type="primary" size="mini"
+          <el-button style="width: 100%" type="primary" @click="login"
             >登陆</el-button
           >
         </el-form-item>
@@ -58,7 +68,12 @@ export default {
         password: "",
       },
     };
-  }
+  },
+  methods: {
+    login() {
+      /* 登录请求 */
+    },
+  },
 };
 </script>
 
