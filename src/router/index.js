d84d252b011e25abfb49f9fe1ad26f5bb4f0c087
xiongleixin <leilei@yscredit.com>
2021-08-18 16:15:01 +0800

    fix: 修复部署路径出错

--- src/router/index.js
+++ src/router/index.js
@@ -75,17 +75,4 @@ router.beforeEach(async (to, from, next) => {
 
 router.afterEach(() => nProgress.done())
 
-/* 路由异常错误处理，尝试解析一个异步组件时发生错误，重新渲染目标页面 */
-router.onError((error) => {
-    const pattern = /Loading chunk (\d)+ failed/g;
-    const isChunkLoadFailed = error.message.match(pattern);
-    const targetPath = router.history.pending.fullPath;
-    if (isChunkLoadFailed) {
-      router.replace(targetPath);
-    }
-});
-
-export {
-    router,
-    configRouters
-}
+export { router, configRouters }
