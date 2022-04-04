4776c53147b93be71d55bfad0b27f0a30f8f5aa2
coderlei <972761675@qq.com>
2022-04-04 14:41:21 +0800

    fix: 权限按钮优化

--- src/directives/permission.js
+++ src/directives/permission.js
@@ -1,5 +1,7 @@
-import store from "@/store"
+import { getUsername } from "@/common/cookie"
+import { queryPermissions } from "@/api/permission"
 
+let permissions = [], hasRequestPermission = false
 /**
  * 判断当前用户是否有权限操作按钮
  * @param {String | Array<String>} permissionCode 
@@ -7,29 +9,32 @@ import store from "@/store"
  */
 function hasPermissions(permissionCode) {
     return new Promise((resolve) => {
-        // 全局事件监听 event bus
-        setTimeout(() => {
-            let { permissions } = store.state.permission;
-            if(typeof permissionCode === 'object') {
-                for (let i = 0, len = permissionCode.length; i < len; i++)
-                    if(!permissions.includes(permissionCode[i])) return resolve(false);
-                resolve(true)
-            }
-            resolve(permissions.includes(permissionCode))
-        }, 100);
+        if (typeof permissionCode === 'object') {
+            for (let i = 0, len = permissionCode.length; i < len; i++)
+                if (!permissions.includes(permissionCode[i])) return resolve(false);
+            resolve(true)
+        }
+        resolve(permissions.includes(permissionCode))
     })
 }
 /* 定义全局权限指令 */
 function defineDirective(Vue) {
     Vue.directive('permission', {
         inserted: async function (el, binding) {
-            if (!await hasPermissions(binding.value)) {
-                el.parentNode.removeChild(el);
+            // 如果不存在就去请求
+            if (!hasRequestPermission) {
+                const ll_username = getUsername();
+                let data = await queryPermissions({ ll_username });
+                permissions = data.permissions
+                hasRequestPermission = true
+                !await hasPermissions(binding.value) && el.parentNode.removeChild(el);
+            } else {
+                !await hasPermissions(binding.value) && el.parentNode.removeChild(el);
             }
+
         }
     })
 }
-
 export default {
     install: (Vue) => {
         defineDirective(Vue);
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_NEV == 'development' ? '/' : './'
+    publicPath: process.env.NODE_NEV == 'development' ? './' : '/'
 }
